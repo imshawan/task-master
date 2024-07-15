172c57f31b33530173d40d92ac5220a33f091e21
Shawan Mandal <github@imshawan.dev>
2024-07-15 23:58:26 +0530

    update:Added 404 route handler

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const middlewares = require('../middlewares');
+const { response } = require('../utilities');
 const router = express.Router();
 
 router.options('/*', middlewares.cors.cors, (req, res) => res.status(200));
@@ -7,5 +8,7 @@ router.options('/*', middlewares.cors.cors, (req, res) => res.status(200));
 router.use('/api/user', middlewares.cors.corsWithDelegate, require('./user'));
 router.use('/api/auth', middlewares.cors.corsWithDelegate, require('./authentication'));
 router.use('/api/task', middlewares.cors.corsWithDelegate, middlewares.authentication.validate, require('./task'));
+
+router.use('/*', (req, res) => response.format(404, res, {message: `Cannot ${req.method} ${req.originalUrl}`}))
 
 module.exports = router;
